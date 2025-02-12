656ec9f54e8c8c305bd379b019e01d40faf04727
Fidahasm <fidahkt@gmail.com>
2025-02-12 13:05:06 +0530

    added some fetching queries

---
+++ sql-queries-t-fetch-data.sql
@@ -0,0 +1,32 @@
+--sql query to fetch data from customer_landing
+select * from customer_landing;
+
+--sql query to fetch data from customer_trusted
+select * from customer_trusted;
+
+--sql query to fetch data from customer_trusted where shareResearchAsOfData is null
+select * from customer_trusted where shareResearchAsOfData = null;
+
+--sql query to fetch data from customer_curated
+select * from customer_curated;
+
+--sql query to fetch data from accelerometer_landing
+select * from accelerometer_landing;
+
+--sql query to fetch data from accelerometer_trusted
+select * from accelerometer_trusted;
+
+--sql query to fetch data from step_trainer_landing
+select * from step_trainer_landing;
+
+--sql query to fetch data from step_trainer_trusted
+select * from step_trainer_trusted;
+
+--sql query to fetch data from machine_learning_curated
+select * from machine_learning_curated;
+
+
+
+
+
+
